c861b40a34b505f2ac8dc74b25dd8442fc675f6f
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-07-02 21:18:16 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -37,8 +37,8 @@ class PostsController < ApplicationController
       author = Author.find_by(id: params[:author_id])
     if author.nil?
       redirect_to authors_path, alert: "Author not found."
-  else
-    @post = author.posts.find_by(id: params[:id])
+    else
+      @post = author.posts.find_by(id: params[:id])
     redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
   end
 else
